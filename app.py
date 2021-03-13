3fc1a5f5250f0c7be021f88d33dae4c0dc448a53
Navid Mashinchi <nmashinchi@gmail.com>
2021-03-12 17:49:48 -0800

    First push

---
+++ app.py
@@ -0,0 +1,14 @@
+# We import the libraries
+import streamlit as st
+
+
+# We create our Streamlit App
+def main():
+   
+    st.set_page_config(layout="wide")
+    
+    st.title("Covid19 - Dashboard")
+    
+   
+if __name__ == '__main__':
+    main()
